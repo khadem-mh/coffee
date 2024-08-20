d8ddfe4bbbe630a27f34e48cfdfc39d4a6e6d503
khadem-mh <khadem13359@gmail.com>
2024-08-20 15:13:29 -0700

    handle logic added new email to newsLetter

--- components/template/Offer/Offer.jsx
+++ components/template/Offer/Offer.jsx
@@ -1,5 +1,37 @@
+import { useState } from "react"
+import route from "@/data/routes"
 
 const Offer = () => {
+
+    const [email, setEmail] = useState("")
+    const emailIsValid = new RegExp('^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}$')
+
+    const handleJoinUserToNewsLetter = async event => {
+        event.preventDefault()
+
+        if (emailIsValid.test(email)) {
+
+            try {
+
+                const res = await fetch(`${route}newsLetters`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ email })
+                })
+
+                res.status = 201 ? alert("SUCCESS") : alert("Faild To Execute")
+                setEmail("")
+
+            } catch (err) {
+                //
+            }
+
+        }
+        else alert("Email is not valid")
+    }
+
 
     return (
         <div className="offer container-fluid my-5 py-5 text-center position-relative overlay-top overlay-bottom">
@@ -9,9 +41,9 @@ const Offer = () => {
                 <h4 className="text-white font-weight-normal mb-4 pb-3">Only for Sunday from 1st Jan to 30th Jan 2045</h4>
                 <form className="form-inline justify-content-center mb-4">
                     <div className="input-group">
-                        <input type="text" className="form-control p-4" placeholder="Your Email" style={{ height: "60px" }} />
+                        <input value={email} onChange={e => setEmail(e.target.value)} type="text" className="form-control p-4" placeholder="Your Email" style={{ height: "60px" }} />
                         <div className="input-group-append">
-                            <button className="btn btn-primary font-weight-bold px-4" type="submit">Sign Up</button>
+                            <button onClick={e => handleJoinUserToNewsLetter(e)} className="btn btn-primary font-weight-bold px-4" type="submit">JOIN</button>
                         </div>
                     </div>
                 </form>
