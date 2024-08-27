9dd5eb5ad864e07ea56c51ffde9703ad9abcf88b
khadem-mh <khadem13359@gmail.com>
2024-08-26 20:42:26 -0700

    finish develope and testing

--- components/modules/Navbar/Navbar.jsx
+++ components/modules/Navbar/Navbar.jsx
@@ -7,9 +7,19 @@ const Navbar = () => {
 
     const router = useRouter()
     const [search, setSerch] = useState("")
+    const [isShow, setIsShow] = useState("hidden")
+    const [isVisible, setIsVisible] = useState("")
 
     useEffect(() => {
         if (router.query?.q) setSerch(router.query.q)
+
+        const handleResize = () => window.innerWidth <= 1400 ? setIsVisible(false) : setIsVisible(true)
+
+        window.addEventListener('resize', handleResize)
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize)
+
     }, [])
 
     const clickHandlerSearch = () => router.push(`/search?q=${search.trim()}`)
@@ -20,12 +30,21 @@ const Navbar = () => {
                 <a href="index.html" className="navbar-brand px-lg-4 m-0">
                     <h1 className="m-0 display-4 text-uppercase text-white">Next-Coffee</h1>
                 </a>
-                <GlobalSearch value={search} setValue={setSerch} onClickSearch={clickHandlerSearch} />
-                <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                {
+                    isVisible &&
+                    <GlobalSearch value={search} setValue={setSerch} onClickSearch={clickHandlerSearch} />
+                }
+                <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse" onClick={() => setIsShow(prev => prev === 'hidden' ? 'show' : 'hidden')}>
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                <div className={`collapse navbar-collapse justify-content-between ${isShow}`} id="navbarCollapse">
                     <div className="navbar-nav ml-auto p-4">
+                        {
+                            !isVisible &&
+                            <div style={{marginBottom: '1rem', marginLeft: '1rem'}}>
+                                <GlobalSearch value={search} setValue={setSerch} onClickSearch={clickHandlerSearch} />
+                            </div>
+                        }
                         <NavLink href="/" className="nav-item nav-link">Home</NavLink>
                         <NavLink href="/about" className="nav-item nav-link">About</NavLink>
                         <NavLink href="/service" className="nav-item nav-link">Service</NavLink>
